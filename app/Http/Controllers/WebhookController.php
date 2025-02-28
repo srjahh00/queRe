b9200dbf9ab800fbc64e8d27265b086c3734f908
jhayramirez-cyberland <joshua@cyberland.edu.sg>
2025-02-28 19:28:23 +0800

    webhook endpoints

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -13,17 +13,17 @@ class WebhookController extends Controller
 
         WebhookEntry::dispatch();
 
-        // $data = $request->all();
+        $data = $request->all();
         
-        // $activationId = data_get($data,'activationId');
-        // $sms = Sms::where('rental_id', $activationId)->firstOrFail();
-        // $sms->update([
-        //     'code' => data_get($data,'code'), 
-        //     'text' => data_get($data,'text'), 
-        //     'message_id' => data_get($data,'messageId'), 
-        //     'country' => data_get($data,'country'), 
-        //     'received_at' => data_get($data,'receivedAt'), 
-        // ]);
-        // return $sms;
+        $activationId = data_get($data,'activationId');
+        $sms = Sms::where('rental_id', $activationId)->firstOrFail();
+        $sms->update([
+            'code' => data_get($data,'code'), 
+            'text' => data_get($data,'text'), 
+            'message_id' => data_get($data,'messageId'), 
+            'country' => data_get($data,'country'), 
+            'received_at' => data_get($data,'receivedAt'), 
+        ]);
+        return $sms;
     }
 }
