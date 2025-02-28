aa42a3826bcf1c6cba75ff994923fa21e5dd6617
jhayramirez-cyberland <joshua@cyberland.edu.sg>
2025-02-28 21:57:47 +0800

    reverb fix

--- config/reverb.php
+++ config/reverb.php
@@ -29,12 +29,12 @@ return [
     'servers' => [
 
         'reverb' => [
-            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
-            'port' => env('REVERB_SERVER_PORT', 8080),
-            'hostname' => env('REVERB_HOST'),
-            'options' => [
-                'tls' => [],
-            ],
+           'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
+        'port' => env('REVERB_SERVER_PORT', 443), 
+        'hostname' => env('REVERB_HOST'),
+        'options' => [
+            'tls' => [],
+        ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
@@ -72,14 +72,14 @@ return [
         'apps' => [
             [
                 'key' => env('REVERB_APP_KEY'),
-                'secret' => env('REVERB_APP_SECRET'),
-                'app_id' => env('REVERB_APP_ID'),
-                'options' => [
-                    'host' => env('REVERB_HOST'),
-                    'port' => env('REVERB_PORT', 443),
-                    'scheme' => env('REVERB_SCHEME', 'https'),
-                    'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
-                ],
+            'secret' => env('REVERB_APP_SECRET'),
+            'app_id' => env('REVERB_APP_ID'),
+            'options' => [
+                'host' => env('REVERB_HOST'),
+                'port' => env('REVERB_PORT', 443),
+                'scheme' => env('REVERB_SCHEME', 'https'),
+                'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+            ],
                 'allowed_origins' => ['*'],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
