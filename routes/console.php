00f08f68899a9b3b70dc825aee92b4746726ae44
jhayramirez-cyberland <joshua@cyberland.edu.sg>
2025-05-01 11:17:08 +0800

    feature : daily usage cron job

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Schedule;
 use App\Console\Commands\SendYesterdaySmsUsageToTelegram;
 
 Schedule::command(SendYesterdaySmsUsageToTelegram::class)
-    ->everyFifteenMinutes()
+    ->dailyAt('5:05')
     ->timezone(config('app.timezone', 'Asia/Singapore'));
